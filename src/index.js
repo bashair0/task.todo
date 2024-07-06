6d5cf5bca37e17170891f0a94f9d2d722198bf3e
bashair0 <bisho5.ba@gmail.com>
2024-07-06 23:31:49 +0800

    Style the header navigation section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import _ from 'lodash'
 import './style.css'
-
-const hello = document.createElement('h1')
-hello.classList.add('red')
-hello.textContent = 'Hello world!'
-
-document.body.appendChild(hello)
