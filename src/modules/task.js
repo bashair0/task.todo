f81d16eec9d2e63fc545011f43dda55e2676c2f3
bashair0 <bisho5.ba@gmail.com>
2024-07-10 15:25:58 +0800

    Style the main and aside sections

---
+++ src/modules/task.js
@@ -0,0 +1,9 @@
+class task {
+  constructor (title, description, dueDate, priority, note) {
+    ;(this.title = title),
+      (this.description = description),
+      (this.dueDate = dueDate),
+      (this.priority = priority),
+      (this.note = note)
+  }
+}
